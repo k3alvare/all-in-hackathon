02b7776e87d57eeabb237d1337e9ceed2f17dbbb
rgenge <rgenge@terra.com.br>
2023-03-25 21:03:57 -0400

    report update

--- javascript/filter.js
+++ javascript/filter.js
@@ -9,21 +9,62 @@ let reports = [
 if (localStorage.getItem("reports")) {
   reports = JSON.parse(localStorage.getItem("reports"));
 }
+document.getElementById("filterbutton1").addEventListener("click", filterSelectionB);
+document.getElementById("filterbutton2").addEventListener("click", filterSelectionT);
+document.getElementById("filterbutton3").addEventListener("click", filterSelectionS);
+document.getElementById("filterbutton4").addEventListener("click", filterSelectionO);
 
 // Function to display posts in the list
+function filterSelectionB() {
+	let reportsList = document.getElementById("incidents-list");
+	reportsList.innerHTML = "";
+	for (let i = 0; i < reports.length; i++) {
+	  let report = reports[i];
+	  let li = document.createElement("li");
+	  if(report.incidentType == "Burglary")
+		li.innerHTML = `<p>${report.incidentType}</p><p>${report.incidentDate}</p><p>${report.incidentSummary}</p>`;
+	  if(report.incidentType == "Burglary")
+	  reportsList.appendChild(li);
 
-function displayReports() {
-  let reportsList = document.getElementById("incidents-list");
-  reportsList.innerHTML = "";
-  for (let i = 0; i < reports.length; i++) {
-    let report = reports[i];
-    let li = document.createElement("li");
-	if(report.incidentType == "theft")
-		li.innerHTML = `<p>${report.incidentType}</p>`;
-    li.innerHTML = `<p>${report.incidentType}</p><p>${report.incidentDate}</p><p>${report.incidentSummary}</p>`;
-    reportsList.appendChild(li);
-  }
-  saveReportsToLocalStorage();
-}
+	}
+	}
+function filterSelectionT() {
+	let reportsList = document.getElementById("incidents-list");
+	reportsList.innerHTML = "";
+	for (let i = 0; i < reports.length; i++) {
+		let report = reports[i];
+		let li = document.createElement("li");
+		if(report.incidentType == "Theft")
+			li.innerHTML = `<p>${report.incidentType}</p><p>${report.incidentDate}</p><p>${report.incidentSummary}</p>`;
+		if(report.incidentType == "Theft")
+		reportsList.appendChild(li);
 
-displayReports();
+		}
+		}
+function filterSelectionS() {
+	let reportsList = document.getElementById("incidents-list");
+	reportsList.innerHTML = "";
+	for (let i = 0; i < reports.length; i++) {
+		let report = reports[i];
+		let li = document.createElement("li");
+		if(report.incidentType == "Sexual Assault")
+			li.innerHTML = `<p>${report.incidentType}</p><p>${report.incidentDate}</p><p>${report.incidentSummary}</p>`;
+		if(report.incidentType == "Sexual Assault")
+			reportsList.appendChild(li);
+
+		}
+		}
+
+function filterSelectionO() {
+	let reportsList = document.getElementById("incidents-list");
+	reportsList.innerHTML = "";
+	for (let i = 0; i < reports.length; i++) {
+		let report = reports[i];
+		let li = document.createElement("li");
+		if(report.incidentType == "Other")
+			li.innerHTML = `<p>${report.incidentType}</p><p>${report.incidentDate}</p><p>${report.incidentSummary}</p>`;
+		if(report.incidentType == "Other")
+			reportsList.appendChild(li);
+
+		}
+		}
